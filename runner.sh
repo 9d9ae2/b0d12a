ffdd358ba5f91f5ed9cc277b62c2b09a72105f30
9d9ae2 <114676028+9d9ae2@users.noreply.github.com>
2023-07-07 00:36:28 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -15,4 +15,4 @@ sudo python3 setup.py install
 cd
 git clone https://github.com/DiscordPhone/softphone
 python3 -m pip install -e softphone
-pip install aiohttp==3.7.4 async-timeout==3.0.1 attrs==19.3.0 cffi==1.14.0 chardet==3.0.4 idna==2.10 idna-ssl==1.1.0 multidict==4.7.6 testresources==latest pycparser==2.20 PyNaCl==1.4.0 python-dotenv==0.14.0 six==1.15.0 sounddevice==0.3.15 typing-extensions==3.7.4.3 websockets==9.1 yarl==1.4.2
+pip install aiohttp==3.7.4 async-timeout==3.0.1 attrs==19.3.0 cffi==1.14.0 chardet==3.0.4 idna==2.10 idna-ssl==1.1.0 multidict==4.7.6 testresources==2.0.1 pycparser==2.20 PyNaCl==1.4.0 python-dotenv==0.14.0 six==1.15.0 sounddevice==0.3.15 typing-extensions==3.7.4.3 websockets==9.1 yarl==1.4.2
